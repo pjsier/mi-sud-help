9dfd78a44712695dc734cf526d4699e3e15dd152
pjsier <pjsier@gmail.com>
2026-01-28 21:25:18 -0500

    add base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
-import solidJs from '@astrojs/solid-js';
+import solidJs from "@astrojs/solid-js"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [solidJs()]
-});
+  base: "/mi-sud-help/",
+  integrations: [solidJs()],
+})
